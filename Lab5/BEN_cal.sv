8a6d9be701477ff674a78686695ace09c744ff02
Alexander Jin <amjin2@illinois.edu>
2022-10-04 16:41:51 -0500

    Update

--- Lab5/BEN_cal.sv
+++ Lab5/BEN_cal.sv
@@ -1,4 +1,30 @@
 module BEN_cal (
+	input [15:0] datapath,
+	input [2:0] nzp,
+	input LD.BEN, LD.CC,
+	output BEN
 );
+
+logic N, Z, P, BEN_R;
+
+always_ff @ (posedge Clk)
+ begin
+	if (LD.CC)
+	 begin
+		N <=
+		Z <=
+		P <=
+	 end
+	if (LD.BEN)
+		BEN <=
+ end
+ 
+ 
+ 
+always_comb
+ begin
+	
+ end
+
 
 endmodule
